c37c15e2322df8c60741af69d96c51b3fa00c068
Matt Henderson <matthen@gmail.com>
2019-08-15 13:17:28 +1000

    Implement max bytes per token

--- tensorflow_text/core/kernels/wordpiece_tokenizer.h
+++ tensorflow_text/core/kernels/wordpiece_tokenizer.h
@@ -41,10 +41,11 @@ class WordpieceVocab {
 
 LookupStatus WordpieceTokenize(
     const absl::string_view& token, const int max_bytes_per_token,
-    const std::string& suffix_indicator, bool use_unknown_token,
-    const std::string& unknown_token, const WordpieceVocab* vocab_map,
-    std::vector<std::string>* subwords, std::vector<int>* begin_offset,
-    std::vector<int>* end_offset, int* num_word_pieces);
+    const int max_bytes_per_subtoken, const std::string& suffix_indicator,
+    bool use_unknown_token, const std::string& unknown_token,
+    const WordpieceVocab* vocab_map, std::vector<std::string>* subwords,
+    std::vector<int>* begin_offset, std::vector<int>* end_offset,
+    int* num_word_pieces);
 
 }  // namespace text
 }  // namespace tensorflow
