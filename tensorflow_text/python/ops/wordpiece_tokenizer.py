f41be4584c78dec1e81d08abb073373a847ea8c4
Matt Henderson <matthen@gmail.com>
2019-08-17 17:02:51 +1000

    Backwards compatability

--- tensorflow_text/python/ops/wordpiece_tokenizer.py
+++ tensorflow_text/python/ops/wordpiece_tokenizer.py
@@ -67,7 +67,8 @@ class WordpieceTokenizer(TokenizerWithOffsets):
     self._suffix_indicator = suffix_indicator
     self._max_bytes_per_word = max_bytes_per_word
     self._max_bytes_per_token = (
-        -1 if max_bytes_per_token is None else max_bytes_per_token)
+        0 if max_bytes_per_token is None
+        else max_bytes_per_token)
     self._token_out_type = token_out_type
     self._unknown_token = unknown_token if unknown_token else '[UNK]'
     self._use_unknown_token = True if unknown_token else False
