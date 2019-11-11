17370ff30d7d024b508d436e2fbed726e49a7abf
broken <34356+broken@users.noreply.github.com>
2019-11-11 12:36:42 -0800

    Bump patch version

--- oss_scripts/pip_package/setup.py
+++ oss_scripts/pip_package/setup.py
@@ -32,7 +32,7 @@ from setuptools.command.install import install
 from setuptools.dist import Distribution
 
 project_name = 'tensorflow-text'
-project_version = '1.15.0'
+project_version = '1.15.1'
 
 
 class BinaryDistribution(Distribution):
