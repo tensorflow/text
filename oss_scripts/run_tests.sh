d11cba5aef24e175ac13ce3bec1bcb745a022be2
rneale <rneale@google.com>
2019-11-14 16:33:36 -0800

    Update README
    PiperOrigin-RevId: 280541591

--- oss_scripts/run_tests.sh
+++ oss_scripts/run_tests.sh
@@ -3,6 +3,5 @@
 set -x  # print commands as they are executed
 set -e  # fail and exit on any command erroring
 
-cd /workspace
 ./oss_scripts/configure.sh
 bazel test --test_output=errors tensorflow_text:all
