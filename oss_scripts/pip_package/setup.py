ed72b8becc05e7e4b8a4f80fd3dcef083b408512
Terry Huang <thuang513@gmail.com>
2020-01-31 15:33:31 -0800

    fix version

--- oss_scripts/pip_package/setup.py
+++ oss_scripts/pip_package/setup.py
@@ -32,7 +32,7 @@ from setuptools.command.install import install
 from setuptools.dist import Distribution
 
 project_name = 'tensorflow-text'
-project_version = '2.1.0-rc0'
+project_version = '2.1.0'
 
 
 class BinaryDistribution(Distribution):
@@ -73,11 +73,11 @@ setup(
     cmdclass={'install': InstallPlatlib},
     distclass=BinaryDistribution,
     install_requires=[
-        'tensorflow>=2.1.0-rc0, <2.2',
+        'tensorflow>=2.1.0, <2.1',
     ],
     extras_require={
         'tensorflow_gpu': [
-            'tensorflow-gpu>=2.1.0-rc0, <2.2',
+            'tensorflow-gpu>=2.1.0, <2.1',
         ],
         'tests': [
             'absl-py',
