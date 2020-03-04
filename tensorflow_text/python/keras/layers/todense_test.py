0dc697a7b58eee4ca2fb36bf526a6896b99cba53
reedwm <reedwm@google.com>
2020-03-03 16:25:29 -0800

    No-op change to test presubmit.
    DO NOT SUBMIT.
    
    PiperOrigin-RevId: 298721043

--- tensorflow_text/python/keras/layers/todense_test.py
+++ tensorflow_text/python/keras/layers/todense_test.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests for ToDense Keras layer."""
+"""Tests for ToDense Keras layer.."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
