b74ad41f7f584157a453285fa95c479626353dc8
Robby Neale <rneale@google.com>
2020-03-16 20:44:46 -0400

    Update version

--- oss_scripts/pip_package/setup.py
+++ oss_scripts/pip_package/setup.py
@@ -32,7 +32,7 @@ from setuptools.command.install import install
 from setuptools.dist import Distribution
 
 project_name = 'tensorflow-text'
-project_version = '2.2.0-rc.0'
+project_version = '2.2.0-rc1'
 
 
 class BinaryDistribution(Distribution):
