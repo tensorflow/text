72ea1ef79ba151ae6925d6913718a8aabfda34a1
Robert Neale <rneale@google.com>
2021-01-15 15:19:10 -0800

    Refactor saved_model.py to make it easier to comment out blocks of related code to identify problems. Also moved out the vocab for Wordpiece due to a tf bug.
    PiperOrigin-RevId: 352093814

--- oss_scripts/model_server/save_models.py
+++ oss_scripts/model_server/save_models.py
@@ -37,8 +37,22 @@ flags.DEFINE_string(
 class TfTextOps(tf.Module):
   """Module for saving TF Text concrete function."""
 
+  def __init__(self):
+    # Vocab table for Wordpiece
+    # Can no longer be created within the tf.function due to http://b/169256108
+    wp_initializer = tf.lookup.KeyValueTensorInitializer(
+        ['i'], [1], key_dtype=tf.string, value_dtype=tf.int64)
+    self.wp_vocab_table = tf.lookup.StaticHashTable(wp_initializer,
+                                                    default_value=-1)
+
   @tf.function
   def __call__(self, x):
+    # Assertion method
+    def assert_check(tensor):
+      return tf.assert_equal(tensor, tf.identity(tensor))
+
+    ### TF Text Ops to verify ###
+    op_deps = []
     # Constrained sequence
     cs_scores = np.array([[10.0, 12.0, 6.0, 4.0], [13.0, 12.0, 11.0, 10.0]])
     cs_input = np.array([cs_scores, cs_scores, cs_scores], dtype=np.float32)
@@ -57,6 +71,7 @@ class TfTextOps(tf.Module):
         cs_input, [2, 2, 2], allowed_transitions=cs_allowed_transitions,
         transition_weights=cs_transition_weights, use_log_space=True,
         use_start_and_end_states=True)
+    op_deps.append(assert_check(constrained_sequence.to_tensor()))
     # Max Spanning Tree
     mst_num_nodes = tf.constant([4, 3], tf.int32)
     mst_scores = tf.constant([[[0, 0, 0, 0],
@@ -69,12 +84,15 @@ class TfTextOps(tf.Module):
                                [9, 9, 9, 9]]],
                              tf.int32)  # pyformat: disable
     (max_spanning_tree, _) = text.max_spanning_tree(mst_num_nodes, mst_scores)
+    op_deps.append(assert_check(max_spanning_tree))
     # Normalize
     normalized = text.case_fold_utf8(['A String'])
     normalized = text.normalize_utf8(normalized)
+    op_deps.append(assert_check(normalized))
     # Regex split
     regex_split = text.regex_split(input=['Yo dawg!'],
                                    delim_regex_pattern=r'\s')
+    op_deps.append(assert_check(regex_split.to_tensor()))
     # Rouge-L
     rl_hypotheses = tf.ragged.constant(
         [['captain', 'of', 'the', 'delta', 'flight'],
@@ -83,6 +101,7 @@ class TfTextOps(tf.Module):
         [['delta', 'air', 'lines', 'flight'],
          ['this', 'concludes', 'the', 'transcript']])
     (rouge_l, _, _) = text.metrics.rouge_l(rl_hypotheses, rl_references)
+    op_deps.append(assert_check(rouge_l))
     # Sentence breaking version 1 (token dependent)
     sb_token_word = [['Welcome', 'to', 'the', 'U.S.', '!', 'Harry'],
                      ['Wu', 'Tang', 'Clan', ';', 'ain\'t', 'nothing']]
@@ -105,12 +124,14 @@ class TfTextOps(tf.Module):
                                              dtype=tf.int64)
     (sentence_breaking, _, _, _) = text.sentence_fragments(
         sb_token_word, sb_token_starts, sb_token_ends, sb_token_properties)
+    op_deps.append(assert_check(sentence_breaking.to_tensor()))
     # Sentence breaking version 2 (StateBasedSentenceBreaker)
     sbv2_text_input = [['Welcome to the U.S.! Harry'],
                        ['Wu Tang Clan; ain\'t nothing']]
     sentence_breaker_v2 = text.StateBasedSentenceBreaker()
     sbv2_fragment_text, _, _ = (
         sentence_breaker_v2.break_sentences_with_offsets(sbv2_text_input))
+    op_deps.append(assert_check(sbv2_fragment_text))
     # Sentencepiece tokenizer
     sp_model_file = (
         'third_party/tensorflow_text/python/ops/test_data/test_oss_model.model')
@@ -122,10 +143,15 @@ class TfTextOps(tf.Module):
     sentencepiece_size = sp_tokenizer.vocab_size()
     sentencepiece_id = sp_tokenizer.id_to_string(1)
     sentencepiece_str = sp_tokenizer.string_to_id('<s>')
+    op_deps.append(assert_check(sentencepiece.to_tensor()))
+    op_deps.append(assert_check(sentencepiece_id))
+    op_deps.append(assert_check(sentencepiece_size))
+    op_deps.append(assert_check(sentencepiece_str))
     # Split merge tokenizer
     sm_tokenizer = text.SplitMergeTokenizer()
     split_merge = sm_tokenizer.tokenize(b'IloveFlume!',
                                         [0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0])
+    op_deps.append(assert_check(split_merge))
     # Split merge from logits tokenizer
     smfl_tokenizer = text.SplitMergeFromLogitsTokenizer()
     split_merge_from_logits = smfl_tokenizer.tokenize(
@@ -149,69 +175,29 @@ class TfTextOps(tf.Module):
                 [18.0, 0.7],  # !: split
             ]
         ])
+    op_deps.append(assert_check(split_merge_from_logits.to_tensor()))
     # Confirm TF unicode_script op that requires ICU works
     tf_unicode_script = tf.strings.unicode_script(
         [ord('a'), 0x0411, 0x82b8, ord(',')])
+    op_deps.append(assert_check(tf_unicode_script))
     # Unicode script tokenizer
     us_tokenizer = text.UnicodeScriptTokenizer()
     unicode_script = us_tokenizer.tokenize(['a string'])
+    op_deps.append(assert_check(unicode_script.to_tensor()))
     # Whitespace tokenizer
     ws_tokenizer = text.WhitespaceTokenizer()
     whitespace = ws_tokenizer.tokenize(['a string'])
+    op_deps.append(assert_check(whitespace.to_tensor()))
     # Wordpiece tokenizer
-    wp_initializer = tf.lookup.KeyValueTensorInitializer(
-        ['i'], [1], key_dtype=tf.string, value_dtype=tf.int64)
-    self.wp_vocab_table = tf.lookup.StaticHashTable(wp_initializer,
-                                                    default_value=-1)
     wp_tokenizer = text.WordpieceTokenizer(self.wp_vocab_table)
     wordpiece = wp_tokenizer.tokenize(['i am'])
+    op_deps.append(assert_check(wordpiece.to_tensor()))
     # Wordshape
     wordshapes = text.wordshape([u'a-b', u'a\u2010b'.encode('utf-8')],
                                 text.WordShape.HAS_PUNCTUATION_DASH)
+    op_deps.append(assert_check(wordshapes))
 
-    # Assertion method
-    def assert_check(tensor):
-      return tf.assert_equal(tensor, tf.identity(tensor))
-
-    # Assertions
-    constrained_sequence_assert = assert_check(constrained_sequence.to_tensor())
-    max_spanning_tree_assert = assert_check(max_spanning_tree)
-    normalized_assert = assert_check(normalized)
-    regex_split_assert = assert_check(regex_split.to_tensor())
-    rouge_l_assert = assert_check(rouge_l)
-    sentence_breaking_assert = assert_check(sentence_breaking.to_tensor())
-    sentence_breaking_v2_assert = assert_check(sbv2_fragment_text)
-    sentencepiece_assert = assert_check(sentencepiece.to_tensor())
-    sentencepiece_id_assert = assert_check(sentencepiece_id)
-    sentencepiece_size_assert = assert_check(sentencepiece_size)
-    sentencepiece_str_assert = assert_check(sentencepiece_str)
-    split_merge_assert = assert_check(split_merge)
-    split_merge_from_logits_assert = assert_check(
-        split_merge_from_logits.to_tensor())
-    tf_unicode_script_assert = assert_check(tf_unicode_script)
-    unicode_script_assert = assert_check(unicode_script.to_tensor())
-    whitespace_assert = assert_check(whitespace.to_tensor())
-    wordpiece_assert = assert_check(wordpiece.to_tensor())
-    wordshapes_assert = assert_check(wordshapes)
-
-    with tf.control_dependencies([constrained_sequence_assert,
-                                  max_spanning_tree_assert,
-                                  normalized_assert,
-                                  regex_split_assert,
-                                  rouge_l_assert,
-                                  sentence_breaking_assert,
-                                  sentence_breaking_v2_assert,
-                                  sentencepiece_assert,
-                                  sentencepiece_id_assert,
-                                  sentencepiece_size_assert,
-                                  sentencepiece_str_assert,
-                                  split_merge_assert,
-                                  split_merge_from_logits_assert,
-                                  tf_unicode_script_assert,
-                                  unicode_script_assert,
-                                  whitespace_assert,
-                                  wordpiece_assert,
-                                  wordshapes_assert]):
+    with tf.control_dependencies(op_deps):
       y = tf.add(x, [1])
     return {'y': y}
 
