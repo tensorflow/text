316f765290ea7daaf1eef02b0886ef8692278a8c
Robert Neale <rneale@google.com>
2021-11-18 14:04:33 -0800

    Fix inclusion of missing libraries for Mac & Windows
    PiperOrigin-RevId: 410893953

--- oss_scripts/run_build.sh
+++ oss_scripts/run_build.sh
@@ -6,7 +6,6 @@ osname="$(uname -s)"
 if [[ $osname == "Darwin" ]]; then
   # Update to macos extensions
   sed -i '' 's/".so"/".dylib"/' tensorflow_text/tftext.bzl
-  sed -i '' 's/*.so/*.dylib/' oss_scripts/pip_package/MANIFEST.in
   perl -pi -e "s/(load_library.load_op_library.*)\\.so'/\$1.dylib'/" $(find tensorflow_text/python -type f)
   export CC_OPT_FLAGS='-mavx'
 fi
--- tensorflow_text/python/ops/whitespace_tokenizer_test.py
+++ tensorflow_text/python/ops/whitespace_tokenizer_test.py
@@ -267,7 +267,7 @@ class WhitespaceTokenizerOpTest(test_util.TensorFlowTestCase):
     self.assertAllEqual(starts, expected_offset_starts)
     self.assertAllEqual(ends, expected_offset_ends)
 
-  def DISABLED_testTfLite(self):
+  def testTfLite(self):
     """Checks TFLite conversion and inference."""
 
     class TokenizerModel(tf.keras.Model):
