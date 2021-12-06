bc542f608d878ca4711adf61b9acf8d61a66c031
sun1638650145 <1638650145@qq.com>
2021-12-06 11:49:47 +0800

    Update Apple Silicon's requires.

--- oss_scripts/pip_package/setup.py
+++ oss_scripts/pip_package/setup.py
@@ -73,6 +73,8 @@ setup(
     cmdclass={'install': InstallPlatlib},
     distclass=BinaryDistribution,
     install_requires=[
+        'tensorflow>=2.7.0, <2.8.0; platform_machine != "arm64" and platform_system != "Darwin"',
+        'tensorflow-macos>=2.7.0, <2.8.0; platform_machine == "arm64" and platform_system == "Darwin"',
         'tensorflow_hub>=0.8.0',
     ],
     extras_require={
