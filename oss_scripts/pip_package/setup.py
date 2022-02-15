tensorflow/text

commit a7e1b5b6cdc4d35eb2849f83eaa670bcc7faf10d

Update version

oss_scripts/pip_package/setup.py
@@ -32,7 +32,7 @@ from setuptools.command.install import install
 from setuptools.dist import Distribution
 
 project_name = 'tensorflow-text'
-project_version = '2.7.0'
+project_version = '2.8.0-rc0'
 
 
 class BinaryDistribution(Distribution):
@@ -73,7 +73,7 @@ setup(
     cmdclass={'install': InstallPlatlib},
     distclass=BinaryDistribution,
     install_requires=[
-        'tensorflow>=2.7.0, <2.8.0; platform_machine != "arm64" or platform_system != "Darwin"',
+        'tensorflow>=2.8.0rc0, <2.9',
         'tensorflow-macos>=2.7.0, <2.8.0; platform_machine == "arm64" and platform_system == "Darwin"',
         'tensorflow_hub>=0.8.0',
     ],

WORKSPACE
@@ -63,10 +63,10 @@ http_archive(
 
 http_archive(
     name = "org_tensorflow",
-    strip_prefix = "tensorflow-2.7.0",
-    sha256 = "249b48ddee927801c7a4f8e5442cf1a3c860f6f46b85a2ff7a78b501507dd561",
+    strip_prefix = "tensorflow-2.8.0-rc0",
+    sha256 = "f57872afe6831b35df591004cee57fe40870a52fc19faf502e89334db7cc6686",
     urls = [
-        "https://github.com/tensorflow/tensorflow/archive/v2.7.0.zip"
+        "https://github.com/tensorflow/tensorflow/archive/v2.8.0-rc0.zip"
     ],
 )
 

oss_scripts/configure.sh
@@ -48,10 +48,10 @@ else
     if [[ x"$(arch)" == x"arm64" ]]; then
       pip install tensorflow-macos==2.7.0
     else
-      pip install tensorflow==2.7.0
+      pip install tensorflow==2.8.0rc0
     fi
   else
-    pip install tensorflow==2.7.0
+    pip install tensorflow==2.8.0rc0
   fi
 fi
 

tensorflow_text/__init__.py
@@ -89,4 +89,4 @@ tflite_registrar.SELECT_TFTEXT_OPS = [
 ]
 
 remove_undocumented(__name__, _allowed_symbols)
-__version__ = "2.7.0"
+__version__ = "2.8.0-rc0"

commit 73ebe08544ef182af1ea2f27624740acd10f07f6

📦️ Fix macOS packaging
The TensorFlow Text package requires use of a matching minor version of TensorFlow. In order to support Apple Silicon macs, Tensorflow Text chooses between the `tensorflow` and `tensorflow-macos` packages automatically based on system architecture. However, I believe that commit a7e1b5b6cdc4d35eb2849f83eaa670bcc7faf10d accidentally wiped out this logic. Additionally, this commit failed to increment the minor version of the `tensorflow-macos` dependency.

This typically isn't a problem for users installing TensorFlow Text using `pip` on non-Apple Silicon devices, since `pip` just tries to find the best set of packages for the current system. However, this _is_ a problem for package managers like Poetry, which create a deterministic lock file allowing a similar set of dependencies to be installed across different systems, modulo any architectural differences. Since Poetry needs to resolve a combination of dependencies that will work for _any_ system, it eagerly tries to identify a version of `tensorflow-macos` that will work on Apple Silicon devices. However, since the minor versions of the `tensorflow` and `tensorflow-macos` dependencies don't match, it's unable to find an appropriate version of `keras` that both can use. As a result, installing `tensorflow-text (>=2.8.0)` using Poetry is currently impossible.

These changes restore the logic to automatically select between `tensorflow` and `tensorflow-macos` based on platform and architecture identifiers, and bump the version of the `tensorflow-macos` dependency to match the minor version of `tensorflow` and `tensorflow-text`.

oss_scripts/pip_package/setup.py
@@ -73,8 +73,8 @@ setup(
     cmdclass={'install': InstallPlatlib},
     distclass=BinaryDistribution,
     install_requires=[
-        'tensorflow>=2.8.0, <2.9',
-        'tensorflow-macos>=2.7.0, <2.8.0; platform_machine == "arm64" and platform_system == "Darwin"',
+        'tensorflow>=2.8.0, <2.9; platform_machine != "arm64" or platform_system != "Darwin"',
+        'tensorflow-macos>=2.8.0, <2.9; platform_machine == "arm64" and platform_system == "Darwin"',
         'tensorflow_hub>=0.8.0',
     ],
     extras_require={