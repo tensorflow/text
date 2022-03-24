7556f6a243f6cee873188d54f2ac5194549efe20
Yilei Yang <yileiyang@google.com>
2022-03-24 13:20:23 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 437071060

--- tensorflow_text/python/ops/regex_split_ops.py
+++ tensorflow_text/python/ops/regex_split_ops.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """This file contains the python libraries for the regex_split op."""
 from tensorflow.python.framework import dtypes
 from tensorflow.python.ops import array_ops
