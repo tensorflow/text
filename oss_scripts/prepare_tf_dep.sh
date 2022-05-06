11682adaad41e3e1b664b262ca843e6ddbf06aec
broken <34356+broken@users.noreply.github.com>
2022-05-05 23:43:04 -0700

    Fix prepare_tf_dep.sh for osx

--- oss_scripts/prepare_tf_dep.sh
+++ oss_scripts/prepare_tf_dep.sh
@@ -8,19 +8,24 @@ elif (which python3) | grep -q "python3"; then
   installed_python="python3"
 fi
 
+ext=""
+if [[ "${osname}" == "darwin" ]]; then
+  ext='""'
+fi
+
 # update setup.nightly.py with tf version
 tf_version=$($installed_python -c 'import tensorflow as tf; print(tf.__version__)')
 echo "$tf_version"
-sed -i "s/project_version = 'REPLACE_ME'/project_version = '${tf_version}'/" oss_scripts/pip_package/setup.nightly.py
+sed -i $ext "s/project_version = 'REPLACE_ME'/project_version = '${tf_version}'/" oss_scripts/pip_package/setup.nightly.py
 # update __version__
-sed -i "s/__version__ = .*\$/__version__ = \"${tf_version}\"/" tensorflow_text/__init__.py
+sed -i $ext "s/__version__ = .*\$/__version__ = \"${tf_version}\"/" tensorflow_text/__init__.py
 
 # Get commit sha of installed tensorflow
 short_commit_sha=$($installed_python -c 'import tensorflow as tf; print(tf.__git_version__)' | tail -1 | grep -oP '(?<=-g)[0-9a-f]*$')
 commit_sha=$(curl -SsL https://github.com/tensorflow/tensorflow/commit/${short_commit_sha} | grep sha-block | grep commit | sed -e 's/.*\([a-f0-9]\{40\}\).*/\1/')
 
 # Update TF dependency to installed tensorflow
-sed -i "s/strip_prefix = \"tensorflow-2\.[0-9]\+\.[0-9]\+\(-rc[0-9]\+\)\?\",/strip_prefix = \"tensorflow-${commit_sha}\",/" WORKSPACE
-sed -i "s|\"https://github.com/tensorflow/tensorflow/archive/v.\+\.zip\"|\"https://github.com/tensorflow/tensorflow/archive/${commit_sha}.zip\"|" WORKSPACE
+sed -E -i $ext "s/strip_prefix = \"tensorflow-2.\+\",/strip_prefix = \"tensorflow-${commit_sha}\",/" WORKSPACE
+sed -E -i $ext "s|\"https://github.com/tensorflow/tensorflow/archive/.\+\.zip\"|\"https://github.com/tensorflow/tensorflow/archive/${commit_sha}.zip\"|" WORKSPACE
 prev_shasum=$(grep -A 1 -e "strip_prefix.*tensorflow-" WORKSPACE | tail -1 | awk -F '"' '{print $2}')
-sed -i "s/sha256 = \"${prev_shasum}\",//" WORKSPACE
+sed -i $ext "s/sha256 = \"${prev_shasum}\",//" WORKSPACE
