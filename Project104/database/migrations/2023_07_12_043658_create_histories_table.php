884ec53efe75ca0e916c20582c06c4cfac6c48d2
dulshan-lakshitha <128488551+dulshan-lakshitha@users.noreply.github.com>
2023-07-18 16:53:31 +0400

    done

---
+++ Project104/database/migrations/2023_07_12_043658_create_histories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')->constrained()->cascadeOnDelete();
+            $table->string("type");
+            $table->string('desciption')->default("");
+            $table->text('sale_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('histories');
+    }
+};
