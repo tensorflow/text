5d57f19e43f9bc79d6d1c35df7f606d473e59e85
TF.Text Team <tf-text-github-robot@google.com>
2024-11-02 19:41:01 -0700

    No public description
    PiperOrigin-RevId: 692583234

--- oss_scripts/pip_package/tensorflow_build_info.py
+++ oss_scripts/pip_package/tensorflow_build_info.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# coding=utf-8
 # Copyright 2024 TF.Text Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
