08d45fe83890ac572ac45633faa5a612b7d489e2
davidhabboosh1 <habboosh.d@northeastern.edu>
2025-08-12 22:16:45 -0400

    use pip-compile rather than bazel

--- oss_scripts/configure.sh
+++ oss_scripts/configure.sh
@@ -76,7 +76,8 @@ if [[ "$TF_VERSION" == *"rc"* ]]; then
   REQUIREMENTS_EXTRA_FLAGS="$REQUIREMENTS_EXTRA_FLAGS --pre"
 fi
 
-bazel run //oss_scripts/pip_package:requirements.update -- $REQUIREMENTS_EXTRA_FLAGS
+pip-compile pip_package/requirements.in -- $REQUIREMENTS_EXTRA_FLAGS
+# bazel run //oss_scripts/pip_package:requirements.update -- $REQUIREMENTS_EXTRA_FLAGS
 
 TF_ABIFLAG=$(bazel run //oss_scripts/pip_package:tensorflow_build_info -- abi)
 SHARED_LIBRARY_NAME="libtensorflow_framework.so.2"
