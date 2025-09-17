a250630be417ecc53015fae449113ee74fb59742
TF.Text Team <tf-text-github-robot@google.com>
2025-09-17 07:45:35 -0700

    Update protobuf version to avoid conflicts with other part of the Tensorflow.
    PiperOrigin-RevId: 808150882

--- oss_scripts/pip_package/setup.py
+++ oss_scripts/pip_package/setup.py
@@ -46,7 +46,7 @@ from setuptools.command.install import install
 from setuptools.dist import Distribution
 
 project_name = 'tensorflow-text'
-project_version = '2.18.0'
+project_version = '2.20.0'
 
 
 class BinaryDistribution(Distribution):
@@ -87,13 +87,11 @@ setup(
     cmdclass={'install': InstallPlatlib},
     distclass=BinaryDistribution,
     install_requires=[
-        (
-            'tensorflow>=2.18.0, <2.19',
-        ),
+        'tensorflow>=2.20.0',
     ],
     extras_require={
         'tensorflow_cpu': [
-            'tensorflow-cpu>=2.18.0, <2.19',
+            'tensorflow-cpu>=2.20.0',
         ],
         'tests': [
             'absl-py',
