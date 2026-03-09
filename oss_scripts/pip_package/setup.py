17a3a3ae92c5e224e6fcd07f98cdd3a676c6cdbc
RaviTeja Gorijala <rtg0795@gmail.com>
2026-03-09 22:47:45 +0000

    Update Version for 2.20.1

--- oss_scripts/pip_package/setup.py
+++ oss_scripts/pip_package/setup.py
@@ -46,7 +46,7 @@ from setuptools.command.install import install
 from setuptools.dist import Distribution
 
 project_name = 'tensorflow-text'
-project_version = '2.20.0'
+project_version = '2.20.1'
 
 
 class BinaryDistribution(Distribution):
